0fda2f5801f01550885d7401961db865464bb76d
jmg5909 <jmg5909@naver.com>
2023-02-08 17:33:36 +0900

    about 80% 완료

--- script/main.js
+++ script/main.js
@@ -2,13 +2,12 @@ const opener = document.querySelector(".opener");
 const closer = document.querySelector("#closer");
 const menu = document.querySelector(".menu");
 
-
 opener.addEventListener("click", function () {
-    menu.classList.remove("hide")
-    console.log("hide 클래스를 제거했습니다")
+  // menu.classList.remove("hide")
+  menu.style.display = "block";
 });
 
 closer.addEventListener("click", function () {
-    menu.classList.add("hide")
-    console.log("hide 클래스를 추가했습니다")
+  // menu.classList.add("hide")
+  menu.style.display = "none";
 });
